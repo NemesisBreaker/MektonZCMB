8742297d48d5a44a526d31e52ea14cc05fffcaa8
NemesisBreaker <penalndarthur@gmail.com>
2017-11-10 14:50:47 -0500

    Fifth Commit: Finish Adding Base Values for Weapons

--- MektonZCMB/Models/Weapon.cs
+++ MektonZCMB/Models/Weapon.cs
@@ -5,12 +5,18 @@ namespace MektonZCMB.Models
     {
 
         public int CP { get; set; }
+        public string Name { get; set; }
         public int Space { get; set; }
         public int Kills { get; set; }
         public double Weight { get; set; }
         public int Range { get; set; }
+        public int Damage { get; set; }
         public int Type { get; set; }
         public int Shots { get; set; }
+        public int BurstValue { get; set; }
+        public int WeaponAccuracy { get; set; }
+        public int DefenseAccuracy { get; set; }
+        public int StoppingPower { get; set; }
         public string Notes { get; set; }
         public Weapon()
         {
