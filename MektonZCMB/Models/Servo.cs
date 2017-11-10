06a8f752f5571230d8b4f36fa6caa637e1083f13
NemesisBreaker <penalndarthur@gmail.com>
2017-11-10 14:37:42 -0500

    Third Commit: Adding Base Weapon and Base Servo Models

---
+++ MektonZCMB/Models/Servo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MektonZCMB.Models
+{
+    public class Servo
+    {
+        public string ServoName { get; set; }
+        public string ServoType { get; set; }
+        public double ServoWeight { get; set; }
+        public double ServoCost { get; set; }
+        public int BaseServoKills { get; set; }
+        public int BaseServoSpace { get; set; }
+        public int CurrentServoKills { get; set; }
+        public int CurrentServoSpace { get; set; }
+        public int Thruster { get; set; }
+        public int GES { get; set; }
+        public Servo()
+        {
+        }
+    }
+}
---
+++ MektonZCMB/Models/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MektonZCMB.Models
+{
+    public class Weapon
+    {
+
+        public int CP { get; set; }
+        public int Space { get; set; }
+        public int Kills { get; set; }
+        public double Weight { get; set; }
+        public int Range { get; set; }
+        public int Type { get; set; }
+        public int Shots { get; set; }
+        public string Notes { get; set; }
+        public Weapon()
+        {
+        }
+    }
+}
